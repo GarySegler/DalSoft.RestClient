8a777e7e698eeaa65a6fba2b28ed233f4689b488
DalSoft <darran.jones@gmail.com>
2017-03-01 01:43:11 +0000

    WIP

---
+++ DalSoft.RestClient.Test.Unit/Handlers/FromUrlEncodedHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DalSoft.RestClient.Handlers;
+using NUnit.Framework;
+
+namespace DalSoft.RestClient.Test.Unit.Handlers
+{
+    public class FromUrlEncodedHandlerTests
+    {
+        private const string FormUrlEncodedContentType = "application/x-www-form-urlencoded";
+
+        [Test]
+        public async Task Test()
+        {
+            HttpRequestMessage actualRequest = null;
+            var httpClientWrapper = new HttpClientWrapper
+            (
+                new Config(new FormUrlEncodedHander(), new UnitTestHandler(request => actualRequest = request))
+            );
+
+            
+            await httpClientWrapper.Send(HttpMethod.Post, new Uri("http://test.test"),
+                new Dictionary<string, string> { { "Content-Type", FormUrlEncodedContentType } }, new
+                {
+                    hello = new
+                    {
+                        complex= new
+                        {
+                            @is="OK"
+                        }
+                    }
+                });
+
+            string formUrlEncoded = actualRequest.Content.ReadAsStringAsync().Result;
+                
+        }
+    }
+}
+
---
+++ DalSoft.RestClient/Handlers/FormUrlEncodedHander.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DalSoft.RestClient.Handlers
+{
+    public class FormUrlEncodedHander : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if(!IsFormUrlEncodedContentType(request))
+                return null;
+
+            var content = request.GetContent();
+
+            if (content == null)
+                return null;
+
+            request.Content = new FormUrlEncodedContent(GetContent(content));
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+
+        /// <summary>Returns a List KeyValuePair to pass into FormUrlEncodedContent supports complex objects People[0]First=Darran&amp;People[0]Last=Darran</summary>
+        private static List<KeyValuePair<string, string>> GetContent(object o, List<KeyValuePair<string, string>> nameValueCollection = null, string prefix = null)
+        {
+            nameValueCollection = nameValueCollection ?? new List<KeyValuePair<string, string>>();
+
+            foreach (PropertyInfo property in o.GetType().GetProperties())
+            {
+                var propertyName = prefix == null ? property.Name : $"{prefix}.{property.Name}";
+                var propertyValue = property.GetValue(o);
+
+                if (propertyValue == null) continue;
+
+                if (IsValueTypeOrPrimitiveOrStringOrDateTimeOrGuid(property.PropertyType.GetTypeInfo()))
+                {
+                    nameValueCollection.Add(new KeyValuePair<string, string>(propertyName, propertyValue.ToString()));
+                }
+                else if (propertyValue is IEnumerable)
+                {
+                    var enumerable = ((IEnumerable)propertyValue).Cast<object>().ToArray();
+
+                    for (var i = 0; i < enumerable.Count(); i++)
+                    {
+                        foreach (var propertyItem in enumerable[i].GetType().GetProperties())
+                        {
+                            var propertyItemName = $"{propertyName}[{i}].{propertyItem.Name}";
+                            var propertyItemValue = propertyItem.GetValue(enumerable[i]);
+
+                            if (propertyItemValue == null) continue;
+
+                            if (IsValueTypeOrPrimitiveOrStringOrDateTimeOrGuid(propertyItem.PropertyType.GetTypeInfo()))
+                            {
+                                nameValueCollection.Add(new KeyValuePair<string, string>(propertyItemName, propertyItemValue.ToString()));
+                            }
+                            else
+                            {
+                                GetContent(propertyItemValue, nameValueCollection, propertyItemName);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    GetContent(property.GetValue(o), nameValueCollection, propertyName);
+                }
+            }
+
+            return nameValueCollection;
+        }
+
+        private static bool IsValueTypeOrPrimitiveOrStringOrDateTimeOrGuid(TypeInfo type)
+        {
+            return type.IsValueType || type.IsPrimitive || type.IsEnum || type.AsType() == typeof(string) || type.AsType() == typeof(DateTime) || type.AsType() == typeof(Guid);
+        }
+
+        private static bool IsFormUrlEncodedContentType(HttpRequestMessage request)
+        {
+            return request.GetContentType() == "application/x-www-form-urlencoded";
+        }
+    }
+}
