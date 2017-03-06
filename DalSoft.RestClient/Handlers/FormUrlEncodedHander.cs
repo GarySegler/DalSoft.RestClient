af68f37501d3f80e19215761d2c7b0096c05e0b3
DalSoft <darran.jones@gmail.com>
2017-03-06 02:42:53 +0000

    Refactored MemberAccessWrapper to use commands as the code was getting a bit untidy.

--- DalSoft.RestClient/Handlers/FormUrlEncodedHander.cs
+++ DalSoft.RestClient/Handlers/FormUrlEncodedHander.cs
@@ -1,89 +1,24 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Reflection;
+﻿using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using DalSoft.RestClient.Extensions;
 
 namespace DalSoft.RestClient.Handlers
 {
-    public class FormUrlEncodedHander : DelegatingHandler
+    internal class FormUrlEncodedHander : DelegatingHandler
     {
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             if (IsFormUrlEncodedContentType(request))
             {
                 var content = request.GetContent();
-                request.Content = content == null ? null : new FormUrlEncodedContent(GetContent(content));
+                request.Content = content == null ? null : new FormUrlEncodedContent(content.ObjectToKeyValuePair());
             }
 
             return await base.SendAsync(request, cancellationToken); //next in the pipeline
         }
 
-        /// <summary>Returns a List KeyValuePair to pass into FormUrlEncodedContent supports complex objects People[0]First=Darran&amp;People[0]Last=Darran</summary>
-        private static List<KeyValuePair<string, string>> GetContent(object o, List<KeyValuePair<string, string>> nameValueCollection = null, string prefix = null, int recrusions=0)
-        {
-            const int maxRecrusions = 100;
-            recrusions = prefix == null ? 0 : recrusions + 1;
-            if (recrusions> maxRecrusions) throw new InvalidOperationException("Object supplied to be UrlEncoded is nested too deeply");
-
-            nameValueCollection = nameValueCollection ?? new List<KeyValuePair<string, string>>();
-
-            foreach (var property in o.GetType().GetProperties())
-            {
-                var propertyName = prefix == null ? property.Name : $"{prefix}.{property.Name}";
-                var propertyValue = property.GetValue(o);
-
-                if (propertyValue == null) continue;
-
-                if (IsValueTypeOrPrimitiveOrStringOrDateTimeOrGuid(property.PropertyType.GetTypeInfo()))
-                {
-                    nameValueCollection.Add(new KeyValuePair<string, string>(propertyName, propertyValue.ToString()));
-                }
-                else if (propertyValue is IEnumerable)
-                {
-                    var enumerable = ((IEnumerable)propertyValue).Cast<object>().ToArray();
-
-                    for (var i = 0; i < enumerable.Length; i++)
-                    {
-                        if (IsValueTypeOrPrimitiveOrStringOrDateTimeOrGuid(enumerable[i].GetType().GetTypeInfo()))
-                            nameValueCollection.Add(new KeyValuePair<string, string>(propertyName, enumerable[i].ToString()));
-                        
-                        foreach (var propertyItem in enumerable[i].GetType().GetProperties())
-                        {
-                            var propertyItemName = $"{propertyName}[{i}].{propertyItem.Name}";
-                            var propertyItemValue = propertyItem.GetValue(enumerable[i]);
-
-                            if (propertyItemValue == null) continue;
-
-                            if (IsValueTypeOrPrimitiveOrStringOrDateTimeOrGuid(propertyItem.PropertyType.GetTypeInfo()))
-                            {
-                                nameValueCollection.Add(new KeyValuePair<string, string>(propertyItemName, propertyItemValue.ToString()));
-                            }
-                            else
-                            {
-                                GetContent(propertyItemValue, nameValueCollection, propertyItemName, recrusions);
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    GetContent(property.GetValue(o), nameValueCollection, propertyName, recrusions);
-                }
-            }
-
-            return nameValueCollection;
-        }
-
-        private static bool IsValueTypeOrPrimitiveOrStringOrDateTimeOrGuid(TypeInfo type)
-        {
-            return type.IsValueType || type.IsPrimitive || type.IsEnum || type.AsType() == typeof(string) || type.AsType() == typeof(DateTime) || type.AsType() == typeof(Guid);
-        }
-
-        private static bool IsFormUrlEncodedContentType(HttpRequestMessage request)
+               private static bool IsFormUrlEncodedContentType(HttpRequestMessage request)
         {
             return request.GetContentType() == "application/x-www-form-urlencoded";
         }
